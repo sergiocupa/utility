26a207e31980c0cefd592ac1656e39987871d9ce
DESKTOP-TLGP5B5\sergio <sergiocupa@outlook.com>
2025-03-01 02:05:24 -0300

    correcao em string_split_param()

--- stringlib/src/stringlib.c
+++ stringlib/src/stringlib.c
@@ -161,11 +161,10 @@ StringArray* string_array_release(StringArray* ar, bool only_data)
 }
 
 
-StringArray* string_split(const char* content, const int length, const char* token, const int token_length, const bool remove_empty)
+void string_split_param(const char* content, const int length, const char* token, const int token_length, const bool remove_empty, StringArray* array)
 {
-	StringArray* array = string_array_new();
-	int pos = -1;
 	int leng = 0;
+	int pos = -1;
 	int ix = 0;
 
 	if (remove_empty)
@@ -217,24 +216,14 @@ StringArray* string_split(const char* content, const int length, const char* tok
 	return array;
 }
 
-void string_split_param(const char* content, const int length, const char* token, const int token_length, StringArray* array)
+StringArray* string_split(const char* content, const int length, const char* token, const int token_length, const bool remove_empty)
 {
-	int pos = -1;
-	int ix = 0;
-	while (ix < length)
-	{
-		pos = string_index_of(content, length, token, token_length, ix);
-
-		if (pos >= 0)
-		{
-			String* str = string_sub_new(content, length, ix, (pos - ix));
-			string_array_add(array, str);
-		}
-		else break;
-		ix = pos + token_length;
-	}
+	StringArray* array = string_array_new();
+	string_split_param(content, length, token, token_length, remove_empty, array);
 	return array;
 }
+
+
 
 StringArray* string_split_first_char(const char* content, const int length, const char* token, const int token_length)
 {
